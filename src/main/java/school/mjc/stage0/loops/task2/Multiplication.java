b47847a1778a9cc839b901aca851994dd7f54d3f
albert-pakichev <69592416+albert-pakichev@users.noreply.github.com>
2023-07-08 16:33:33 +0500

    Update Multiplication.java

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+        int factorial = 1;
+        int multiplyByAndToInclusive = 7;
+        int result = 1;
+
+        if(multiplyByAndToInclusive<1) {
+            System.out.println(1);
+        }
+        else {
+            while (factorial <= multiplyByAndToInclusive) {
+                factorial++;
+                result = result * factorial;
+                System.out.println(result);
+            }
+        }
     }
 }
