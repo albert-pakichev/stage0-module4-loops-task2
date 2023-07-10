65cac422b503c8f1f1bc5d23221eccffce238980
albert-pakichev <69592416+albert-pakichev@users.noreply.github.com>
2023-07-10 21:07:34 +0500

    Update FactorialNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,10 +2,12 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-        int factorial = 0;
+    int factorial = 1;
+    BigInteger result = BigInteger.ONE;
         while(factorial<printToInclusive) {
-            factorial++;
-            System.out.println(factorial);
+            int i = factorial++;
+            result = result.multiply(BigInteger.valueOf(i));
+            System.out.println(result);
         }
     }
 }
