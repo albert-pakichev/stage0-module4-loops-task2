6f502305c704dd5df1d9b11cd43bcb5371d4e334
albert-pakichev <69592416+albert-pakichev@users.noreply.github.com>
2023-07-10 18:11:53 +0500

    Update WordsBuilder.java

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,33 +2,13 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
-char symbol = 0;
-        int i = symbol;
-    while (i<127) {
-        symbol++;
-        if(symbol == '!') {
-            String str = String.valueOf(symbol);
-            System.out.print(str);
-            continue;
-        } else if (symbol == 'H') {
-            String str = String.valueOf(symbol);
-            System.out.print(str);
-            continue;
+     int i = 0;
+        while (i < chars.length) {
+            char c = chars[i];
+            System.out.print(c);
+            i++;
         }
-        else if (symbol == 'I') {
-            String str = String.valueOf(symbol);
-            System.out.print(str);
-            continue;
-        }
-        else if (symbol == 'N') {
-            String str = String.valueOf(symbol);
-            System.out.print(str);
-            continue;
-        } else if (symbol == 'T') {
-            String str = String.valueOf(symbol);
-            System.out.print(str);
-            break;
-        }
-    }
+        System.out.println();
+    
     }
 }
